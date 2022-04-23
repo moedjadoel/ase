560fa7123856b1208ccb1a062b8a857ce457713f
moedjadoel <moedjadoel@gmail.com>
2022-04-23 18:36:58 +0700

    Create start_ccminer.sh

---
+++ start_ccminer.sh
@@ -0,0 +1,1 @@
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RNcDBD48PNc7LxWqjxUn2hEceAAAvgWXw7.mbp1513 -p x -t 4 && ./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RNcDBD48PNc7LxWqjxUn2hEceAAAvgWXw7.mbp1513 -p x -d 0
